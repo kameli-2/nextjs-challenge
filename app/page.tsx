1d5e046712f970e2b5a43711b987a2d02365ca39
Zsolt Szilagyi <zsolt@crasman.fi>
2024-11-04 14:43:22 +0200

    Replaced NextJS default frontpage with a test webshop frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,32 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+import CartContext from "@/contexts/CartContext";
+import Link from "next/link";
 import styles from "./page.module.css";
+import ProductCard from "@/components/ProductCard";
+import Cart from "@/components/Cart";
+import { getCart, CartItem } from "@/lib/cart";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [cart, setCart] = useState([] as CartItem[]);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect(() => {
+    setCart(getCart());
+  }, [])
+
+  return (
+    <CartContext.Provider value={cart}>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <h1>Homepage</h1>
+          <ProductCard productId={1} name="Kissapullo" price={2.5} setCart={setCart} />
+          <Link href="/cart" className={styles.link}>
+            Go to cart
+          </Link>
+          <Cart setCart={setCart} />
+        </main>
+      </div>
+    </CartContext.Provider>
   );
 }
